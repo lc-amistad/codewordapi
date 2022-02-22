0c2042633b29221ae4c655a1719fc14bdf0fed80
AlejandroG1 <jalegalarza@gmail.com>
2022-02-22 06:08:06 -0500

    media url fix

--- codewordapi/urls.py
+++ codewordapi/urls.py
@@ -1,11 +1,10 @@
-
-from cgitb import handler
-from xml.dom.minidom import Document
 from django.conf import settings
 from django.contrib import admin
 from rest_framework import routers
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
+
 from api import views
 
 router = routers.DefaultRouter()
@@ -20,6 +19,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
 
 handler404 = 'utils.views.error_404'
 handler500 = 'utils.views.error_500'
