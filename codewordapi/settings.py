cd4aa405fdba91b70a53e6ca088dc88ee24a7ea3
AlejandroG1 <jalegalarza@gmail.com>
2022-02-22 05:51:19 -0500

    media

--- codewordapi/settings.py
+++ codewordapi/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
